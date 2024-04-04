572febfd20cba47db1c4a5e808c99d3df93643a7
goofy foot <williamk31@gmail.com>
2024-04-03 21:26:14 -0600

    added login function and userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,59 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // make a post route to '/' to create a new user
+router.post('/', async (req, res) => {
+    try{
+        const userData = await User.create(req.body);
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
 // make a post route to '/login' to verify email and password for existing user
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json({ user: userData, message: 'Welcome!' });
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err);
+    }
+});
 
 //make a post route to '/logout' to logout user
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
