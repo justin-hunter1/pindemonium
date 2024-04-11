12b81a5d579a4df0e7cf0d7a33d9c633eb87cf35
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-04-11 18:16:33 -0500

    commiting before merge

--- public/js/addMachine.js
+++ public/js/addMachine.js
@@ -14,7 +14,7 @@ const addMachineHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/profile')
+            document.location.replace('/')
         } else {
             alert(response.statusText);
         }
